fe34b8c0342b935233099aeb62558fc467233f45
Djordje <djordjemilic33@gmail.com>
2023-02-20 20:42:03 +0100

    add: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div>Hey there</div>;
-}
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+
+const App = () => {
+  Aos.init({
+    duration: 1800,
+    offset: 100,
+  });
+  return (
+    <div className=" overflow-hidden">
+      <Header />
+      <Hero />
+    </div>
+  );
+};
 
 export default App;
